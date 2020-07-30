07f128287ac00a316373724d9071276c9035bd13
xuyi1997 <xuyibit@163.com>
2020-07-30 14:52:25 +0200

    Add files via upload

---
+++ apiconfig.py
@@ -0,0 +1,5 @@
+api_key = "wg62dhqm2bfuuxzygvt5r5wz"
+api_secret = "AYqEEFoWrNw4haX79WgW"
+username = "info@squla.nl"
+password = "W3lk0m01"
+refreshtokendata = 'sl/LQ6zr9hM51/VCuNttmbx2Exs/tYITcvt5VqWmyxgPrbAUTyycVbiWq42vhQTyM4z0Nrth+/yF0o91oCOK5hgDzgVDpiHJrfzQKk1IkuR7jnCWr72qohoXMIoQ3/uqqiq7qXPcvVRZAwldsz6fhaNesOROuw7XpEvC5QTqI08=|77u/YVZVQjlTZGtXckgyRVRlQUFCU1YKMTAwNzI0CjczMTU4MjkKM2JUV0VRPT0KNWJ2V0VRPT0KMQp3ZzYyZGhxbTJiZnV1eHp5Z3Z0NXI1d3oKODAuMTEyLjE3OC4yMDgKMAoKS1RxNEV3PT0KNDQxCjAKCgo=|3|4|1'
